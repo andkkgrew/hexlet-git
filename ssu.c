3c78524e1b381482b0c14c81867890888350c728
andkkgrew <andkkgrew@gmail.com>
2022-01-30 22:48:52 +0700

    attempt2

--- ssu.c
+++ ssu.c
@@ -2,12 +2,5 @@
 
 void main() {
 	printf("Hello, World!\n');
-}#include <stdio.h>
 
-void main() {
-	printf("Hello, World!\n');
-}#include <stdio.h>
-
-void main() {
-	printf("Hello, World!\n');
 }
