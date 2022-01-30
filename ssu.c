e843995d6e8b48ee5d5deec5055af8c2e5ca55df
andkkgrew <andkkgrew@gmail.com>
2022-01-30 22:46:45 +0700

    trying

---
+++ ssu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	printf("Hello, World!\n');
+}#include <stdio.h>
+
+void main() {
+	printf("Hello, World!\n');
+}#include <stdio.h>
+
+void main() {
+	printf("Hello, World!\n');
+}
